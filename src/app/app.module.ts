32c70fd6cd7631c6fb19724225d091ea963f458e
Tom Wanzek <tomwanzek@gmail.com>
2017-05-28 19:56:41 -0400

    Add flex-layout. Import key modules for layout, ui, animation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MdButtonModule,
+  MdCardModule,
+  MdSidenavModule,
+  MdToolbarModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +20,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdButtonModule,
+    MdCardModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
