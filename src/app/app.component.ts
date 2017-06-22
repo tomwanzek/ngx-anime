7b49f70f4d0b25d604bf94dbb27a27cc79967e33
Tom Wanzek <tomwanzek@gmail.com>
2017-06-22 13:58:49 -0400

    Github repo reference. Uppercase. * Add reference link to github repo. Closes #10. * Use text-transform to uppercase button text.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NG Animation Tests';
+  title = 'Angular Animation Tests';
   darkTheme = false;
   sideNavOpen = false;
 
